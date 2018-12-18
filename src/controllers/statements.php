7dc4e2cb84259d3482e5cfb024123ea950cf5b3a
carloshans1 <carlosoliveira@cobra002079.saovicente.local>
2018-12-18 09:35:51 -0200

    Organizando data início e data fim do extrato

--- src/controllers/statements.php
+++ src/controllers/statements.php
@@ -10,7 +10,27 @@ use Psr\Http\Message\ServerRequestInterface;
 
 
 $app
-    ->get('/statements', function() use($app) {
+    ->get('/statements', function(ServerRequestInterface $request) use($app) {
         $view = $app->service('view.renderer');
-        return $view->render('statements.html.twig');
+        //$repository = $app->service('statement.repository');
+        //$auth = $app->service('auth');
+        $data = $request->getQueryParams();    
+
+        //$dateStart = isset($_GET['date_start'])?$_GET['date_start']:new \DateTime();
+        $dateStart = $data['date_start'] ?? (new \DateTime())->modify('-1 month');
+        $dateStart = $dateStart instanceof \DateTime ? $dateStart->format('Y-m-d')
+        : \DateTime::createFromFormat('d/m/Y', $dateStart)->format('Y-m-d');
+
+        $dateEnd = $data['date_end'] ?? new \DateTime();
+        $dateEnd = $dateEnd instanceof \DateTime ? $dateEnd->format('Y-m-d')
+        : \DateTime::createFromFormat('d/m/Y', $dateEnd)->format('Y-m-d');
+        
+        //$statements = $repository->all($dateStart, $dateEnd, $auth->user()->getId());
+        print_r($dateStart);print_r($dateEnd);die();
+
+        return $view->render(
+            'statements.html.twig', [
+            'statements' => $statements
+            ]
+        );
     }, 'statements.list');
