acb946d28780e81e1906da179b13a7fefddc0ac9
carloshans1 <carlosoliveira@cobra002079.saovicente.local>
2018-12-26 13:03:36 -0200

    Gerando gráfico de gastos

--- public/index.php
+++ public/index.php
@@ -2,11 +2,11 @@
 
 use Psr\Http\Message\ServerRequestInterface;
 use SONFin\Application;
+use SONFin\Plugins\AuthPlugin;
+use SONFin\Plugins\DbPlugin;
 use SONFin\Plugins\RoutePlugin;
 use SONFin\Plugins\ViewPlugin;
 use SONFin\ServiceContainer;
-use SONFin\Plugins\DbPlugin;
-use SONFin\Plugins\AuthPlugin;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -32,6 +32,7 @@ $app->get('/home/{name}/{id}', function (ServerRequestInterface $request) {
 });
 
 //Incluindo o arquivo de rotas
+require_once __DIR__ . '/../src/controllers/charts.php';
 require_once __DIR__ . '/../src/controllers/statements.php';
 require_once __DIR__ . '/../src/controllers/category-costs.php';
 require_once __DIR__ . '/../src/controllers/bill-receives.php';
