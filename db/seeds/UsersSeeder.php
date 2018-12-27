ac7113c7762134eee3dd2ffe7cd32f32721175a4
carloshans1 <carlosoliveira@cobra002079.saovicente.local>
2018-12-27 17:02:24 -0200

    Iniciando deploy no Heroku

--- db/seeds/UsersSeeder.php
+++ db/seeds/UsersSeeder.php
@@ -20,15 +20,16 @@ class UsersSeeder extends AbstractSeed
         $auth = $app->service('auth');
         
         $faker = \Faker\Factory::create('pt_BR');
+        //usuario administrador definido direto na seed
         $users = $this->table('users');
-        $users->insert([
+        /*$users->insert([
             'first_name' => $faker->firstName,
             'last_name' => $faker->lastName,
             'email' => 'admin@user.com',
             'password' => $auth->hashPassword('123456'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
-        ])->save();
+        ])->save();*/
         $data = [];
         foreach (range(1, 3) as $value) {
             $data[] = [
--- src/Application.php
+++ src/Application.php
@@ -24,77 +24,65 @@ class Application
     /**
      * Application constructor.
      *
-     * @param $_serviceContainer  
+     * @param $serviceContainer
      */
-    
-    public function __construct(ServiceContainerInterface $_serviceContainer)
+    public function __construct(ServiceContainerInterface $serviceContainer)
     {
-        $this->serviceContainer = $_serviceContainer;
+        $this->_serviceContainer = $serviceContainer;
     }
-
     public function service($name)
     {
-        return $this->serviceContainer->get($name);
+        return $this->_serviceContainer->get($name);
     }
-
     public function addService(string $name, $service): void
     {
-        if(is_callable($service)) {
-            $this->serviceContainer->addLazy($name, $service);
+        if (is_callable($service)) {
+            $this->_serviceContainer->addLazy($name, $service);
         } else {
-            $this->serviceContainer->add($name, $service);
+            $this->_serviceContainer->add($name, $service);
         }
     }
-
     public function plugin(PluginInterface $plugin): void
     {
-        $plugin->register($this->serviceContainer);
+        $plugin->register($this->_serviceContainer);
     }
-
     public function get($path, $action, $name = null): Application
     {
         $routing = $this->service('routing');
         $routing->get($name, $path, $action);
         return $this;
     }
-
     public function post($path, $action, $name = null): Application
     {
         $routing = $this->service('routing');
         $routing->post($name, $path, $action);
         return $this;
     }
-
-    public function redirect($path):ResponseInterface
+    public function redirect($path): ResponseInterface
     {
         return new RedirectResponse($path);
     }
-
     public function route(string $name, array $params = []): ResponseInterface
     {
         $generator = $this->service('routing.generator');
         $path = $generator->generate($name, $params);
         return $this->redirect($path);
     }
-
     public function before(callable $callback): Application
     {
-        array_push($this->befores, $callback);
+        array_push($this->_befores, $callback);
         return $this;
     }
-
-    protected function runBefores(): ? ResponseInterface
+    protected function runBefores(): ?ResponseInterface
     {
-        foreach ($this->befores as $callback) {
+        foreach ($this->_befores as $callback) {
             $result = $callback($this->service(RequestInterface::class));
-            if($result instanceof ResponseInterface) {
+            if ($result instanceof ResponseInterface) {
                 return $result;
             }
         }
-
         return null;
     }
-
     public function start(): void
     {
         $route = $this->service('route');
@@ -102,35 +90,28 @@ class Application
          * @var ServerRequestInterface $request
          */
         $request = $this->service(RequestInterface::class);
-
         if (!$route) {
-            echo "Página não encontrada";
+            echo "Page not found";
             exit;
         }
-
         foreach ($route->attributes as $key => $value) {
             $request = $request->withAttribute($key, $value);
         }
-
         $result = $this->runBefores();
-        if($result) {
+        if ($result) {
             $this->emitResponse($result);
             return;
         }
-
         $callable = $route->handler;
         $response = $callable($request);
         $this->emitResponse($response);
     }
-
-    protected function emitResponse(ResponseInterface $response):void
+    protected function emitResponse(ResponseInterface $response): void
     {
         $emitter = new SapiEmitter();
         $emitter->emit($response);
     }
 }
-
-
 /**
  * Lógica - função -> resposta ou redirecionamento
  * Lógica - função -> resposta ou redirecionamento
